588555b60c65b03b65a807e0891361c96cf82eef
Brent Hanover <brenthanover@gmail.com>
2019-06-07 22:08:04 -0700

    fixed deleted page file

---
+++ Workout-App/imports/ui/Page.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import TrainerCard from './TrainerCard';
+import TrainersMenu from './TrainersMenu';
+import TrainerRow from './TrainerRow';
+import TrainerInfo from './TrainerInfo';
+
+
+export default class Page extends React.Component {
+
+
+
+    render() {
+        if (this.props.value === 'Trainers') {
+            return (
+                <div>
+                    <TrainersMenu />
+                    <br />
+                    <TrainerCard />
+                </div>
+            );
+        }
+        if (this.props.value === 'Trainer') {
+            return (
+                <div>
+                    <br/>
+                    <TrainerRow />
+                    <br />
+                    <TrainerInfo/>
+                </div>
+            );
+        }
+        if (this.props.value === 'Gym') {
+            return (
+                <div>
+                    <GymMenu/>
+                    <br/>
+                    <GymCard/>
+                </div>
+            )
+        } else {
+                return (
+                    <div>
+                        <p>to be implemented</p>
+                    </div>
+                );
+            }
+        }
+    }
+}

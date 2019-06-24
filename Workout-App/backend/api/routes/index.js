c48478a1a13932b5b170a53e7ad354f471e452fe
Oiver Yan Li <oliverlee1003@outlook.com>
2019-06-24 14:12:26 -0700

    add express and mongosse in backend, now we can see trainers in the mongo database

---
+++ Workout-App/backend/api/routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+// var mongoose = require('mongoose');
+
+// const dbRoute =
+//     'mongodb+srv://m001-student:m001-mongodb-basics@sandbox-wrjir.mongodb.net/test?retryWrites=true&w=majority'
+
+// mongoose.connect(dbRoute, {dbName: 'workoutApp'});
+
+// var trainerSchema = new mongoose.Schema(
+//   {
+//     "firstName": String,
+//     "lastName": String,
+//     "gender": String,
+//     "gym": String,
+//     "description": String,
+//     "email": String,
+//     "tags":[
+//       String
+//     ],
+//     "cost": Number,
+//     "overall_rate": Number,
+//     "comments":[
+//       {     
+//         "fullname" : String,
+//         "context": String,
+//         "date": Date,
+//         "rate": Number
+//       }
+//   ]
+//   },
+//   { collection : "trainers"}
+// )
+
+// var Trainer = mongoose.model("Trainer", trainerSchema)
+
+// Trainer.find({},function(err,trainers){
+//   if(err){
+//     console.log("Oh Error!");
+//     console.log(err);
+//   } else {
+//     console.log("All the trainers");
+//     console.log(trainers);
+//   }
+// }
+
+
+// )
+
+// mongoose.connect('')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+// [
+// {
+//   "firstName": "Rock",
+//   "lastName": "Li",
+//   "gender": "male",
+//   "gym": "Ron Zalko's Fitness",
+//   "description": "Rock works out 2 times a day for hours and eats 5000 calories to stay THE ROCK",
+//   "email": "therock@gamil.com",
+//   "tags":[
+//     "power", 
+//     "arm",
+//     "cardio",
+//     "chest"
+//   ],
+//   "cost": 30.99,
+//   "overall_rate": 9.5,
+//   "comments":[
+//     {     
+//       "fullname" : "Stevie Feliciano",
+//       "context": "Hey guys, I highly recommend The Rock as a trainer. He's very friendly and professionl",
+//       "date": "2019-01-01",
+//       "rate": 10
+//     },
+//     {     
+//       "fullname" : "Danny Rose",
+//       "context": " Expensive but worthy it!",
+//       "date": "2019-02-01",
+//       "rate": 10
+//   }
+// ]
+// }
+// ]
--- Workout-App/imports/ui/components/App.jsx
+++ Workout-App/imports/ui/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SideMenu from './SideMenu';
 import Header from './Header';
 
@@ -7,13 +7,34 @@ import currentpageReducer from './../reducers/page.js'
 import { createStore } from 'redux';
 
 const store = createStore(currentpageReducer);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
 
-const App = () => (
+callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+
+render(){
+  return (
   <div>
-      <Header />
-      <br />
-      <Provider store={store}><SideMenu /></Provider>
+  <Header />
+  <br />
+  <Provider store={store}><SideMenu /></Provider>
   </div>
-);
+  )
+  
+}
+
+ 
+};
 
 export default App;

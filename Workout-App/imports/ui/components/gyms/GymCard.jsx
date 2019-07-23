5e3ef8a3287e48c24f2ddc3639d6080d8be6aee8
Oiver Yan Li <oliverlee1003@outlook.com>
2019-07-23 15:03:17 -0700

    revert GymCard

--- Workout-App/imports/ui/components/gyms/GymCard.jsx
+++ Workout-App/imports/ui/components/gyms/GymCard.jsx
@@ -3,130 +3,112 @@ import { connect } from 'react-redux'
 
 class GymCard extends React.Component {
 
-  constructor(props) {
+    constructor(props) {
+        super(props);
+    }
 
-    super(props);
-    
-  }
+    sendTagToParent = (tag) => {
+        console.log("sending tag to parent");
+        console.log(tag);
+        this.props.parentCallBack(tag);
+    };
 
-  sendTagToParent = (tag) => {
-    console.log("sending tag to parent");
-    console.log(tag);
-    this.props.parentCallBack(tag);
-  };
+    render() {
+        var now = new Date();
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var todayIndex = now.getDay();
 
-  render() {
-    var now = new Date();
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var todayIndex = now.getDay();
-
-    return ( <
-      div >
-      <
-      div className = "ui divided items" >
-      <
-      div className = "item" >
-      <
-      div className = "ui large image" >
-      <
-      img src = { this.props.gym.picture } alt = "" / >
-      <
-      /div> <
-      div className = "content" >
-      <
-      a className = "header" > { this.props.gym.name } < /a> <
-      div className = "meta" >
-      <
-      span className = "cinema" > { this.props.gym.description } <
-      /span> <
-      /div>
+        return (
+            <div>
+                <div className="ui divided items">
+                    <div className="item">
+                        <div className="ui large image">
+                            <img src={ this.props.gym.picture } alt=""/>
+                        </div>
+                        <div className="content">
+                            <a className="header">{ this.props.gym.name }</a>
+                            <div className="meta">
+                                <span className="cinema">
+                                    { this.props.gym.description }
+                                </span>
+                            </div>
 
 
-      <
-      div className = "ui primary button" >
-      See trainers at { this.props.gym.name } <
-      i className = "right chevron icon" / >
-      <
-      /div> <
-      div className = "content" >
-      <
-      div className = "extra" > {
-        this.props.gym.tags.map((tag) => {
-          return ( <
-            button className = "ui button"
-            onClick = {
-              () => this.sendTagToParent(tag) } >
-            { tag } <
-            /button>
-          );
-        })
-      } <
-      /div>
+                            <div className="ui primary button">
+                                See trainers at { this.props.gym.name }
+                                <i className="right chevron icon"/>
+                            </div>
+                            <div className="content">
+                                <div className="extra">
+                                    {
+                                        this.props.gym.tags.map((tag) => {
+                                            return (
+                                                <button
+                                                    className="ui button"
+                                                    onClick={ () => this.sendTagToParent(tag) }
+                                                >
+                                                { tag }
+                                            </button>
+                                            );
+                                        })
+                                    }
+                                </div>
 
-      <
-      div className = "bottom aligned" >
-      <
-      p > { this.props.gym.spiel } <
-      /p> <
-      /div> <
-      /div> <
-      /div>
+                                <div className="bottom aligned">
+                                    <p>
+                                        { this.props.gym.spiel }
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
 
 
 
-      <
-      div className = "content" >
-      <
-      div className = "ui card" >
-      <
-      div className = "image medium" >
-      <
-      img src = "https://i.imgur.com/WMq5Sux.png" / >
-      <
-      /div> <
-      div className = "content" >
-      <
-      a className = "header" > { this.props.gym.name } <
-      /a> <
-      div className = "meta" >
-      <
-      span className = "date" >
-      Open { days[todayIndex] } { this.props.gym.hours[todayIndex] } <
-      /span> <
-      /div> <
-      div className = "description" > { this.props.gym.description } <
-      /div> <
-      /div> <
-      div className = "extra content" >
-      <
-      a >
-      <
-      i className = "user icon" / >
-      22 swolr members checked in here <
-      /a> <
-      /div> <
-      /div> <
-      /div> <
-      /div>
+                        <div className="content">
+                            <div className="ui card">
+                                <div className="image medium">
+                                    <img src="https://i.imgur.com/WMq5Sux.png"/>
+                                </div>
+                                <div className="content">
+                                    <a className="header">
+                                        { this.props.gym.name }
+                                    </a>
+                                    <div className="meta">
+                                        <span className="date">
+                                            Open { days[todayIndex] } { this.props.gym.hours[todayIndex] }
+                                        </span>
+                                    </div>
+                                    <div className="description">
+                                        { this.props.gym.description }
+                                    </div>
+                                </div>
+                                <div className="extra content">
+                                    <a>
+                                        <i className="user icon" />
+                                        22 swolr members checked in here
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-      <
-      /div> <
-      /div>
+                </div>
+            </div>
 
-    )
-  }
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
+    return {
 
-  };
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    addTag: (tag) => dispatch(addTag(tag))
-  };
+    return {
+        addTag: (tag) => dispatch(addTag(tag))
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(GymCard);

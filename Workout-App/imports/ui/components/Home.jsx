70578d0225bc53db60b075705a561c05acff3574
Brent Hanover <brenthanover@gmail.com>
2019-07-30 22:47:58 -0700

    push to master

--- Workout-App/imports/ui/components/Home.jsx
+++ Workout-App/imports/ui/components/Home.jsx
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                THIS IS THE HOME PAGE
+                THIS IS THE HOME PAGE!
             </div>
         )
     }
